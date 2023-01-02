6458745b0213a27688bbb14e8da53991640c4b11
Alana <you@example.com>
2023-01-02 14:01:53 +0300

    new

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("error, argc < 2");
+        return 0;
+    }
+
+    int i = 0;
+    int ii = 0;
+
+
+        while (argv[i][ii] != '\0') {
+            ii++;
+        }
+
+        if (ii == 1) {
+            printf("\n 1");
+        }
+
+        else printf("\n 2");
+
 }
